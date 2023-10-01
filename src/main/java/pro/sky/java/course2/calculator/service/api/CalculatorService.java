119deae7ed08f0bd117aa1e78a3a5a53cde45771
Mr-john056 <ekulakov056@gmail.com>
2023-10-01 17:10:16 +0500

    Finish homeWork16

---
+++ src/main/java/pro/sky/java/course2/calculator/service/api/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculator.service.api;
+
+public interface CalculatorService {
+    String showWelcome();
+
+    String summa(int usnum1, int usnum2);
+
+    String minus(int usnum1, int usnum2);
+
+    String multiply(int usnum1, int usnum2);
+
+    String divide(int usnum1, int usnum2);
+}
---
+++ src/main/java/pro/sky/java/course2/calculator/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,29 @@
+package pro.sky.java.course2.calculator.service.impl;
+
+import org.springframework.stereotype.Service;
+import pro.sky.java.course2.calculator.service.api.CalculatorService;
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    public String showWelcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String summa(int usnum1, int usnum2) {
+        return usnum1 + " + " + usnum2 + " = " + (usnum1 + usnum2);
+    }
+
+    public String minus(int usnum1, int usnum2) {
+        return usnum1 + " - " + usnum2 + " = " + (usnum1 - usnum2);
+    }
+
+    public String multiply(int usnum1, int usnum2) {
+        return usnum1 + " * " + usnum2 + " = " + (usnum1 * usnum2);
+    }
+
+    public String divide(int usnum1, int usnum2) {
+        if (usnum2 == 0) {
+            return "Ошибка: на ноль делить нельзя!";
+        } else
+            return usnum1 + " / " + usnum2 + " = " + (usnum1 / usnum2);
+    }
+}
